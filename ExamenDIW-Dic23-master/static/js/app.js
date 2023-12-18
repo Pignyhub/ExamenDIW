6c3a0d0b9874700e31308a98c7ad62563e911f4b
Ignacio <71664853+Pignyhub@users.noreply.github.com>
2023-12-18 10:57:13 +0100

    Add files via upload

---
+++ ExamenDIW-Dic23-master/static/js/app.js
@@ -0,0 +1,17 @@
+console.log("Hola mundo...");
+
+function abrirModal() {
+    document.getElementById('miModal').style.display = 'block';
+  }
+
+  function cerrarModal() {
+    document.getElementById('miModal').style.display = 'none';
+  }
+
+  function abrirModal2() {
+    document.getElementById('miModal2').style.display = 'block';
+  }
+
+  function cerrarModal2() {
+    document.getElementById('miModal2').style.display = 'none';
+  }
